f2d0aa3eb82e0132ef7ede4a02deb88a648b3453
Nik White <underscore.nik@gmail.com>
2014-03-03 15:26:01 -0600

    remove closure

--- mixture.js
+++ mixture.js
@@ -1,64 +1,59 @@
 /**
  * Mixture object composition library
  */
-(function(){
+
+function mix( ) {
 	
-	function mix( ) {
-		
-		var _slice = Array.prototype.slice;
-		var _mixins = _slice.call(arguments);
-		var _inheritable = null;
-		var _state = {};
-		
-		/**
-		 * Extend implementation nabbed from underscore.js 1.6.0
-		 */
-		function extend( obj ) {
-			var sources = _slice.call(arguments, 1);
-			sources.forEach( function(source) {
-				if (source) {
-					for (var prop in source) {
-						obj[prop] = source[prop];
-					}
+	var _slice = Array.prototype.slice;
+	var _mixins = _slice.call(arguments);
+	var _inheritable = null;
+	var _state = {};
+	
+	/**
+	 * Extend implementation nabbed from underscore.js 1.6.0
+	 */
+	function extend( obj ) {
+		var sources = _slice.call(arguments, 1);
+		sources.forEach( function(source) {
+			if (source) {
+				for (var prop in source) {
+					obj[prop] = source[prop];
 				}
-			});
-			return obj;
-		}
-		
-		return {
-			
-			/**
-			 * Inject state information to a priviledged trait/mixin
- 			 * @param {Object} state Key/value pairs 
-			 */
-			'using' : function using( state ) {
-				
-				_state = state ;
-				
-				return this;
-			},
-			
-			
-			/**
-			 * Apply mixin and state to a reference object
-			 * @param {Object} context
-			 * @return {Object} context with mixins and state applied
-			 */
-			'into': function into( context ) {
-				context = context || {};
-				
-				var combined = {};
-				
-				_mixins.forEach(function(mixin){
-					extend( combined, typeof mixin === 'function' ? new mixin(_state) : mixin );		
-				});
-				
-				return Object.create(combined);
 			}
-			
-		};
+		});
+		return obj;
 	}
 	
-	window.mix = mix;
-	return mix;
-})();
+	return {
+		
+		/**
+		 * Inject state information to a priviledged trait/mixin
+		 * @param {Object} state Key/value pairs 
+		 */
+		'using' : function using( state ) {
+			
+			_state = state ;
+			
+			return this;
+		},
+		
+		
+		/**
+		 * Apply mixin and state to a reference object
+		 * @param {Object} context
+		 * @return {Object} context with mixins and state applied
+		 */
+		'into': function into( context ) {
+			context = context || {};
+			
+			var combined = {};
+			
+			_mixins.forEach(function(mixin){
+				extend( combined, typeof mixin === 'function' ? new mixin(_state) : mixin );		
+			});
+			
+			return Object.create(combined);
+		}
+		
+	};
+}
